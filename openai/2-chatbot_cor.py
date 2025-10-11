c27499d0dd71f065ef469e128de7b4e86c36e8d1
E1257010 Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2025-10-11 14:17:04 -0300

    Explorando prompt templates

--- openai/2-chatbot_cor.py
+++ openai/2-chatbot_cor.py
@@ -11,24 +11,26 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 # Inicializando o colorama
 init(autoreset=True)
 
+
 def geracao_texto(mensagens):
     resposta = client.chat.completions.create(
         messages=mensagens,
         model="gpt-3.5-turbo-0125",
         max_tokens=1000,
         temperature=0,
-        stream=True
+        stream=True,
     )
     print(f"{Fore.CYAN}Bot: ", end="")
-    texto_completo=""
+    texto_completo = ""
     for resposta_stream in resposta:
         texto = resposta_stream.choices[0].delta.content
         if texto:
             print(texto, end="")
-            texto_completo+=texto
+            texto_completo += texto
     print()
     mensagens.append({"role": "assistant", "content": texto_completo})
     return mensagens
+
 
 if __name__ == "__main__":
     print(f"{Fore.YELLOW}Bem-vindo ao chatbot👺🤖")
