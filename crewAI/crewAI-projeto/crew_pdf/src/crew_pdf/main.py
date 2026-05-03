e0c384e74f53de5129d9ef24a97cd86c12723568
C1353253 Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2026-05-03 20:56:43 -0300

    Utilizando Tool de Pesquisa em PDF

---
+++ crewAI/crewAI-projeto/crew_pdf/src/crew_pdf/main.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+import sys
+import warnings
+
+from datetime import datetime
+
+from crew_pdf.crew import CrewPdf
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# This main file is intended to be a way for you to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        "pergunta": """
+        Quais são os direitos que a LGPD garante ao titular dos dados? 
+        Em quais artigos isso está detalhado e como uma empresa deve 
+        atender a esses direitos?
+        """,
+    }
+
+    try:
+        CrewPdf().crew().kickoff(inputs=inputs)
+    except Exception as e:
+        raise Exception(f"An error occurred while running the crew: {e}")
+
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {"topic": "AI LLMs", "current_year": str(datetime.now().year)}
+    try:
+        CrewPdf().crew().train(
+            n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs
+        )
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
+
+
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        CrewPdf().crew().replay(task_id=sys.argv[1])
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    inputs = {"topic": "AI LLMs", "current_year": str(datetime.now().year)}
+
+    try:
+        CrewPdf().crew().test(
+            n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs
+        )
+
+    except Exception as e:
+        raise Exception(f"An error occurred while testing the crew: {e}")
+
+
+run()
